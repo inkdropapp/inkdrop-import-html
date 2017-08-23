50034395f7b23b45c75307da828266de8373401a
Takuya Matsuyama <nora@odoruinu.net>
2017-08-23 18:02:48 +0900

    Add compiled sources

---
+++ src/importer.js
@@ -0,0 +1,62 @@
+import path from 'path'
+import fs from 'fs'
+import { remote } from 'electron'
+import toMarkdown from 'to-markdown'
+const { dialog } = remote
+const { Note } = inkdrop.models
+
+export function openImportDialog () {
+  return dialog.showOpenDialog({
+    title: 'Open HTML file',
+    multiSelections: true,
+    filters: [
+      { name: 'HTML Files', extensions: [ 'html' ] }
+    ]
+  })
+}
+
+export function importHTMLFromMultipleFiles (files, destBookId) {
+  try {
+    for (let i = 0; i < files.length; ++i) {
+      importHTMLFromFile(files[i], destBookId)
+    }
+  } catch (e) {
+    inkdrop.notifications.addError('Failed to import the HTML file', { detail: e.stack, dismissable: true })
+  }
+}
+
+export async function importHTMLFromFile (fn, destBookId) {
+  if (!destBookId) {
+    throw new Error('Destination notebook ID is not specified.')
+  }
+  const html = fs.readFileSync(fn, 'utf-8')
+  const title = path.basename(fn, path.extname(fn))
+  const body = convertToMarkdown(html)
+  const note = new Note({ title, body })
+  note.bookId = destBookId
+  await note.save()
+}
+
+export function convertToMarkdown (html) {
+  const md = toMarkdown(html, { converters: [
+    {
+      filter: ['div', 'p'],
+      replacement (innerHTML) {
+        return '\n' + innerHTML + '\n'
+      }
+    },
+    {
+      filter: ['span'],
+      replacement (innerHTML) {
+        return innerHTML
+      }
+    },
+    {
+      filter: ['img'],
+      replacement (innerHTML, node) {
+        return '' // not supported, yet
+      }
+    }
+  ]})
+  return md
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import SelectBookDialog from './select-book-dialog'
+
+module.exports = {
+  activate () {
+    inkdrop.components.registerClass(SelectBookDialog)
+    inkdrop.layouts.addComponentToLayout('modals', 'SelectBookDialog')
+  },
+
+  deactivate () {
+    inkdrop.layouts.removeComponentFromLayout('modals', 'SelectBookDialog')
+    inkdrop.components.deleteClass(SelectBookDialog)
+  }
+}
